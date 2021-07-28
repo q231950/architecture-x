30dfc134ddbc2847d097e1b30ebd020e49492c1d
Martin Kim Dung-Pham <martinkim.pham@gmail.com>
2021-07-28 21:11:19 +0200

    Add custom TitledHostingController to be able to show titles on pushed view controllers

--- Sources/ArchitectureX/Navigator.swift
+++ Sources/ArchitectureX/Navigator.swift
@@ -18,8 +18,8 @@ public struct Navigator {
     }
 
     func push<C: Coordinator>(_ coordinator: C) {
-        let hostedCoordinatorView = UIHostingController(rootView: coordinator.view)
-        navigationController.show(hostedCoordinatorView, sender: nil)
+        let hostedCoordinatorView = TitledHostingController(rootView: coordinator.view)
+        navigationController.pushViewController(hostedCoordinatorView, animated: true)
     }
 
     func present<C: Coordinator>(_ coordinator: C) {
@@ -40,5 +40,40 @@ public struct Navigator {
 
     func dismiss<C: Coordinator>(_ coordinator: C) {
         coordinator.navigator.navigationController.dismiss(animated: true, completion: nil)
+    }
+}
+
+class TitledHostingController<T: View>: UIHostingController<T> {
+
+    override func willMove(toParent parent: UIViewController?){
+        super.willMove(toParent: parent)
+
+        if parent == nil{
+            self.navigationController?.isToolbarHidden = false
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let rootMirror = Mirror(reflecting: rootView)
+
+        if let title = rootMirror.descendant("title") as? String {
+            navigationItem.title = title
+        }
+
+        if let displayMode = rootMirror.descendant("displayMode") as? NavigationBarItem.TitleDisplayMode {
+            navigationItem.largeTitleDisplayMode = displayMode.uikitDisplayMode
+        }
+    }
+}
+
+private extension NavigationBarItem.TitleDisplayMode {
+    var uikitDisplayMode: UINavigationItem.LargeTitleDisplayMode {
+        switch self {
+        case .automatic: return .automatic
+        case .inline: return .never
+        case .large: return .always
+        @unknown default: return .automatic
+        }
     }
 }
